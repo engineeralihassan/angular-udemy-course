48be50f0a6ccca97f1da86fc75684a4c167285d6
Ali Hassan <engineralihassan@gmail.com>
2023-07-26 11:35:56 +0500

    The observables sectionis complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { Component1Component } from './basics/component1/component1.component';
 import { AppComponent } from './app.component';
 import { DatabindingComponent } from './basics/components/databinding/databinding.component';
-
+// routing files
 const routes: Routes = [
 
   {path:"sections/basics",component:Component1Component},
